e88906d40d60314f810b44704ba15fe7dcc71a57
Marcus Gartner <marcus@lob.com>
2015-12-06 18:58:06 -0800

    added benchmarks for BMP and GIF

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ var path     = require('path');
 var Promise  = require('bluebird');
 var pexec    = Promise.promisify(require('child_process').exec);
 
-var calipers = require('calipers')('png', 'jpeg', 'pdf');
+var calipers = require('calipers')('png', 'jpeg', 'pdf', 'gif', 'bmp');
 
 var ITERATIONS  = 500;
 var CONCURRENCY =  50;
@@ -16,6 +16,8 @@ var CONCURRENCY =  50;
 var PDF_PATH  = path.resolve(__dirname, 'images/123x456.1.pdf');
 var PNG_PATH  = path.resolve(__dirname, 'images/123x456.png');
 var JPEG_PATH = path.resolve(__dirname, 'images/123x456.jpg');
+var GIF_PATH  = path.resolve(__dirname, 'images/245x260.gif');
+var BMP_PATH  = path.resolve(__dirname, 'images/233x143.bmp');
 
 function runCalipersBenchmark (name, file) {
   var arr = new Array(ITERATIONS);
@@ -59,4 +61,16 @@ runCalipersBenchmark('PDF calipers', PDF_PATH)
 })
 .then(function () {
   return runExecBenchmark('JPEG identify', 'identify', JPEG_PATH);
+})
+.then(function () {
+  return runCalipersBenchmark('GIF calipers', GIF_PATH);
+})
+.then(function () {
+  return runExecBenchmark('GIF identify', 'identify', GIF_PATH);
+})
+.then(function () {
+  return runCalipersBenchmark('BMP calipers', BMP_PATH);
+})
+.then(function () {
+  return runExecBenchmark('BMP identify', 'identify', BMP_PATH);
 });
